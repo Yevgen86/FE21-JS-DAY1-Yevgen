5a6f334df2f2d54126d6eaf388bee7c3090a6c40
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-09-22 12:37:16 +0200

    Advanced 1.1 ready

---
+++ Advanced/script.js
@@ -0,0 +1,20 @@
+// SUM of variables
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+var sumAdd = a+b+Number(c)+Number(d)+e;
+console.log("The sum of + = " + sumAdd);
+
+// Multiply variables
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+var sumMulti = Number(f)*g*h*Number(i);
+console.log("The sum of * = " + sumMulti);
+
+console.log("The sum of / = " + sumAdd/sumMulti);
