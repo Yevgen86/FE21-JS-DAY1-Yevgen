571daced04b7f45c9c357f8e2ed0742b826afdf7
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-09-22 11:02:44 +0200

    Basic exercise 1.1 ready

---
+++ Basic/script.js
@@ -0,0 +1,6 @@
+console.log("Hello from the Console");
+document.write("Hello from the Browser <br>");
+// END OF BASIC 1.1
+
+console.log("Hi my name is Martin");
+document.write("Hi my name is Martin");
