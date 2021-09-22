9d7317863ab93eefa2c8dec07a4ebd097c8d9705
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-09-22 12:47:44 +0200

    Advanced 1.2 ready

--- Advanced/script.js
+++ Advanced/script.js
@@ -18,3 +18,22 @@ var sumMulti = Number(f)*g*h*Number(i);
 console.log("The sum of * = " + sumMulti);
 
 console.log("The sum of / = " + sumAdd/sumMulti);
+//////// END OF Advanced 1.1
+
+
+//////// Advanced 1.2
+var multiArray = [ 
+    [1, 2, 1, 24], 
+    [8, 11, 9, 4], 
+    [7, 0, 7, 27], 
+    [7, 4, 28, 14], 
+    [3, 10, 25, 7], 
+    [21, 4, 6, 17], 
+    [3, 5, 26, 3] 
+];
+console.log(multiArray[1][1]);
+console.log(multiArray[4][2]);
+console.log(multiArray[5][3]);
+console.log(multiArray[2][1]);
+//////// END OF Advanced 1.2
+
