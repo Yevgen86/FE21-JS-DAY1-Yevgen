537523405ca0e1d895f1d8dd9a493ae2553f735b
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-09-22 12:05:15 +0200

    Intermediate Exercises ready

---
+++ Intermediate/script.js
@@ -0,0 +1,23 @@
+var names = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+console.log(names.sort());
+// Intermediate 1.1 END
+
+var fruits = ["apple", "banana", "kiwi"];
+fruits.push("orange");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+var animals = ["dog", "horse", "monkey"];
+console.log(animals.sort());
+animals.unshift("cat");
+console.log(animals);
+// END OF Intermediate 1.2
+
+
+var fruitString = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+console.log(fruitString);
+var tempFruit = new Array();
+tempFruit = fruitString.split("/");
+console.log(tempFruit.join("\n"));
+document.write(tempFruit.join("<br>"));
