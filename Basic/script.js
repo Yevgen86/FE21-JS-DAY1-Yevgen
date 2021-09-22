629c047abe30671b9606d914d07d815d95b47e00
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-09-22 11:13:32 +0200

    Basic 1.4 ready

--- Basic/script.js
+++ Basic/script.js
@@ -5,3 +5,7 @@ document.write("Hello from the Browser <br>");
 console.log("Hi my name is Martin");
 document.write("Hi my name is Martin");
 // END OF BASIC 1.2
+
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log("The most valuable player of the match is "+players[3]);
+// END OF BASIC 1.4
